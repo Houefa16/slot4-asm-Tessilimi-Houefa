7e297839e8bfc68ba3a370cbd2f3cc8036b9c3d9
Houefa16 <houefa.tessilimi@epitech.eu>
2024-03-07 10:42:14 +0100

    push function

---
+++ fac.asm
@@ -0,0 +1,32 @@
+    global my_func
+_start:
+
+my_func:
+    xor rax, rax
+    cmp byte[rdx], 'A'
+    jl out
+    cmp byte[rdx], 'a'
+    jl out
+    cmp byte[rdx], 'Z'
+    jg out
+    cmp byte[rdx], 'z'
+    jg out
+    jmp search
+    
+search:
+    mov r11b, byte[rdx]
+    mov r12b, byte[rdi + rax]
+    cmp r11b, r12b
+    je find
+    cmp r11b, 0
+    je out
+    inc rax
+    jmp search
+
+find:
+    ret
+    
+out:
+    xor rax, rax
+    mov rax, -84
+    ret
